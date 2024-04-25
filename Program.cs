d0c92fb5f63e152a95490529e6e3a99725bdc88a
charlesperrillat <ce.perrillat@gmail.com>
2024-04-25 17:46:37 +0200

    uploading the program on GitHub

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _1_List;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> stringCollection = new List<string>();
+
+        stringCollection.Add("Hello");
+        stringCollection.Add("Hi");
+        stringCollection.Add("Bonjour");
+
+        stringCollection.RemoveAt(0);
+        stringCollection.Remove("Bonjour");
+        stringCollection[0] = "Salut";
+
+        for (int i = 0; i < stringCollection.Count; i++)
+            Console.WriteLine(stringCollection[i]);
+    }
+}
+
